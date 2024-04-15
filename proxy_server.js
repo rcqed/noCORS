8fef075c6d3b54f24005c6e8b713554f064b8c0f
rcqed <40952828+rcqed@users.noreply.github.com>
2024-04-15 15:36:02 +0800

    Add files via upload

--- proxy_server.js
+++ proxy_server.js
@@ -2,20 +2,42 @@ const http = require('http');
 const url = require('url');
 const request = require('request');
 const fs = require('fs');
+const cors = require('cors'); // 引入cors模块
 
 // HTTP代理服务器的地址
 const proxyServer = 'http://192.168.44.2:10811';
 
+// 允许的 Referer 列表
+const allowedReferers = [
+  'https://www.test.com/',
+  'http://anotherwebsite.com/'
+];
+
 // 创建一个 HTTP 服务器
 http.createServer((req, res) => {
+  // 设置跨域头
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   // 解析请求中的 URL 和参数
   const { pathname, query } = url.parse(req.url, true);
 
-  // 获取要代理的 URL 和是否使用代理的参数
+  // 获取要代理的 URL 和 Referer
   const proxyUrl = query.url;
+  const referer = req.headers.referer;
+  
+  // 检查是否使用代理
   const useProxy = query.proxy === '1';
 
-  // 根据是否使用代理来设置请求选项
+  // 检查 Referer 是否在允许的 Referer 列表中
+  if (referer && !allowedReferers.some(allowedReferer => referer.startsWith(allowedReferer))) {
+    res.writeHead(403);
+    res.end('Forbidden');
+    return;
+  }
+
+  // 设置请求选项
   const requestOptions = {
     url: proxyUrl,
     encoding: null // 设置为null以获取原始的二进制数据
@@ -36,12 +58,13 @@ http.createServer((req, res) => {
     // 将文件内容作为响应返回给客户端
     const filename = proxyUrl.substring(proxyUrl.lastIndexOf('/') + 1);
     const contentType = response.headers['content-type'];
+
+    // 设置正确的 Content-Type
     res.writeHead(200, {
-      'Content-Type': contentType,
-      'Content-Disposition': `attachment; filename="${filename}"`
+      'Content-Type': contentType
     });
     res.end(body);
   });
-}).listen(6006, '192.168.44.3', () => {
-  console.log('Proxy server is running at http://192.168.44.3:6006/');
+}).listen(6006, '0.0.0.0', () => {
+  console.log('Proxy server is running at http://0.0.0.0:6006/');
 });
